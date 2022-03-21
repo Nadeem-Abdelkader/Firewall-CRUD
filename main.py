5d756b2f20546a275be46a1af4e6d999f7480267
nabd0018 <nabd0018@student.monash.edu>
2022-03-22 00:07:44 +0200

    commented main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 """
+Firewall Configuration Application
+- CRUD operations (Create, Read, Update, Delete)
+- Export to CSV file
+- Import from CSV file
+
+Created by: Nadeem Abdelkader on 15/3/2022
+Last updated by Nadeem Abdelkader on 22/3/2022
+
+MySQL for the database
+Tkinter used to create a simple GUI
+
 """
 # !/usr/bin/env python3
 
